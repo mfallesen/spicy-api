8e3f139def252d2eb4ad5da7fbaa1faa95478e1b
Mike Fallesen <mfallesen2010@gmail.com>
2021-01-14 17:53:43 -0800

    inital server setup

---
+++ server.js
@@ -0,0 +1,39 @@
+// ================
+// Dependencies
+// ================
+const express = require('express');
+const passport = require('passport');
+const helmet = require('helmet');
+// const db = require("./models");
+
+
+const app = express();
+require('dotenv').config();
+
+// ================
+// Static Directory
+// ================
+app.use(express.static(--__dirname + '/public'));
+
+// ================
+// Data Parsing
+// ================
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+
+// require("./auth/auth.js");
+
+// Need Cors?
+
+
+var PORT = process.env.PORT || 3030;
+
+// ================
+// Sequelize sync and start server
+// ================
+
+// Just start server for now wait till models are done. 
+
+app.listen(PORT, function() {
+    console.log("Server running on PORT " + PORT);
+})
