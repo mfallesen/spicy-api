9e318a0c213be2ebdbef7216ce28fa05f475fbcd
Mike Fallesen <mfallesen2010@gmail.com>
2021-01-19 13:56:59 -0800

    resolve POST request when complete

--- controllers/addSpice.js
+++ controllers/addSpice.js
@@ -3,20 +3,27 @@ const router = express.Router();
 const db = require("../models");
 
 router.post('/addSpice', (req, res, next) => {
+    // console.log("Console request: ", req);
             db.Spice.findOne({
                 where: {
                     spice_name: req.body.spice_name,
+                    brand: req.body.brand
                 },
-            }).then((spice) => {        
+            }).then((spice) => { 
+                console.log(spice);       
                 if (!spice) {
                     db.Spice.create({
                         spice_name: req.body.spice_name,
 
                         brand: req.body.brand
                     });
+                    return message = 'Spice Entered into DB'
+                } else {
+                    return message = "Spice already exists in DB"
                 }
             }).then( () => {
-                res.status(200);
+                console.log("sent!");
+                res.status(200).send(message);
             })
             .catch((err) => {
                 console.error(err);  /******/
