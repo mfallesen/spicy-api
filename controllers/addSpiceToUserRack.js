f244cc9ba3de8d8abd57b377403552a3fc56d5ab
Mike Fallesen <mfallesen2010@gmail.com>
2021-01-19 15:10:27 -0800

    route to add a spice to the rack

---
+++ controllers/addSpiceToUserRack.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const router = express.Router();
+const db = require("../models");
+
+router.post('/addSpiceToUserRack', (req, res, next) => {
+
+    db.spiceRack.create({
+        purchase_date: req.body.purchase_date,
+        expiration_date: req.body.expiration_date,
+        spiceId: req.body.spiceId,
+        userId: req.body.userId
+    }).then(() => {
+        console.log("sent!");
+        let message = `'Spice Entered into User ${req.body.userId}'s spice rack'`
+        res.status(200).send(message);
+    })
+        .catch((err) => {
+            console.error(err);  /******/
+            res.status(500).json(err);
+        });
+
+});
+
+module.exports = router
