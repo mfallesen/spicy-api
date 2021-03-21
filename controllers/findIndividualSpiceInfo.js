a14f83af8e98b68792cb0ab2e4cfa9fca5606b2c
Mike Fallesen <mfallesen2010@gmail.com>
2021-03-20 20:42:12 -0700

    add route to find indivdual spice informations

---
+++ controllers/findIndividualSpiceInfo.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const router = express.Router();
+const db = require("../models");
+
+router.post('/findIndividualSpiceInfo', async (req, res, next) => {
+    console.log('hitting route');
+    console.log(req.body.id)
+
+    await db.Spice.findAll({
+        where: {
+            id: req.body.id
+        },
+    })
+        .then((spice) => {
+            res.status(200).json(spice)
+        })
+        .catch((err) => {
+            res.status(500).json(err, req);
+        })
+})
+
+module.exports = router;
